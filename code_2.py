298ff3bf43da3b1d04357ac712b36297a060e480
Геннадий <140153539+tangogenlu@users.noreply.github.com>
2024-06-09 09:58:54 +0300

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,14 +1,14 @@
-
+    return data
     for item in data:
 
-import random
-
-def main():
-
-    main()
-    data = generate_random_data()
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
 if __name__ == "__main__":
-    return data
+
+
+    data = generate_random_data()
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+        print(f"Random Number: {item}")
+import random
+def main():
